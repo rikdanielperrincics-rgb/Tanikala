9c4a6544e7f70da39201a20cc0a87540eea933ce
Jemel Zapanta <jemel.zapanta.cics@ust.edu.ph>
2026-05-19 21:18:12 +0800

    login

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
 import EyeIcon from "./EyeIcon";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 function LoginForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log("Signing in with:", email, password);
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
     }
 
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("Logging in with:", email, password);
+        axios.post("http://localhost:8000/api/login", { email, password })
+            .then(response => {
+                toast.success("Login successful!");
+                setTimeout(() => {
+                    localStorage.setItem("token", response.data.token);
+                    window.location.href = "/";
+                }, 1500);
+            })
+            .catch(error => {
+                toast.error("Login failed. Please try again. \n " + (error.response ? error.response.data.error : error.message));
+            });
+    }
     return (
             <form className="w-full max-w-[80vh]" onSubmit={handleSubmit}>
                 <label className="block text-[17px] font-extrabold text-[#7d7b9b] mb-2 text-left">
@@ -20,9 +40,9 @@ function LoginForm() {
                 <div className="flex items-center border-[1.5px] border-[#d8d8d8] rounded-full mb-[18px] overflow-hidden focus-within:border-[#a78bda] transition-colors">
                 <input
                     type="email"
-                    placeholder="Enter your email"
+                    placeholder="Email"
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={handleEmailChange}
                     required
                     className="flex-1 border-none outline-none px-5 py-[13px] text-[17px] font-medium text-[#333] bg-transparent placeholder:text-[#ccc]"
                 />
@@ -37,7 +57,7 @@ function LoginForm() {
                     type={showPassword ? "text" : "password"}
                     placeholder="Enter your password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={handlePasswordChange}
                     required
                     className="flex-1 border-none outline-none px-5 py-[13px] text-[17px] font-medium text-[#333] bg-transparent placeholder:text-[#ccc]"
                 />
